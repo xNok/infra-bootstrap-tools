f4524f4a6d8e57624a6a99ae95abb86a171df6cd
xNok <nokwebspace@gmail.com>
2022-03-29 01:20:00 +0200

    docs: backup of the example code

---
+++ .articles/4_terraform_cloud_101/digitalocean/main.tf
@@ -0,0 +1,13 @@
+
+/**
+ * # DigitalOcean Project
+ *
+ * Projects let you organize your DigitalOcean resources 
+ * (like Droplets, Spaces, and load balancers) into groups.
+ */
+resource "digitalocean_project" "infra-bootstrap-tools" {
+  name        = "infra-bootstrap-tools"
+  description = "Startup infra for small self-hosted project"
+  purpose     = "IoT"
+  environment = "Development"
+}
---
+++ .articles/4_terraform_cloud_101/digitalocean/versions.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    digitalocean = {
+      source = "digitalocean/digitalocean"
+      version = "2.17.1"
+    }
+  }
+}
+
+provider "digitalocean" {
+  # Configuration options
+}
