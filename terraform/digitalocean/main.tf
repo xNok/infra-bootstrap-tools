8b2321f08ddcc666f154192ee0b5884b18393ec9
xNok <nokwebspace@gmail.com>
2022-03-29 02:03:14 +0200

    feat: create digitalocean droplets

--- terraform/digitalocean/main.tf
+++ terraform/digitalocean/main.tf
@@ -10,4 +10,24 @@ resource "digitalocean_project" "infra-bootstrap-tools" {
   description = "Startup infra for small self-hosted project"
   purpose     = "IoT"
   environment = "Development"
+  resources = digitalocean_droplet.node.*.urn
+}
+
+data "digitalocean_ssh_key" "infra" {
+  name = "infra-bootstrap-tools"
+}
+
+resource "digitalocean_droplet" "node" {
+  count = 2
+
+  image  = "ubuntu-20-04-x64"
+  name   = "node${count.index}"
+  region = "lon1"
+  size   = "s-1vcpu-1gb"
+
+  ssh_keys = [data.digitalocean_ssh_key.infra.id]
+}
+
+output "nodes_ip" {
+  value = digitalocean_droplet.node.ipv4_address
 }
