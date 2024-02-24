a6ad9197f67efeb34f44c36f679d9aca8a04607a
Alexandre Couedelo <nokwebspace@gmail.com>
2024-02-24 21:09:47 +0000

    fix: caddy var fle interpolation

--- ansible/roles/docker-swarm-app-caddy/assets/docker-entrypoint.sh
+++ ansible/roles/docker-swarm-app-caddy/assets/docker-entrypoint.sh
@@ -1,32 +1,27 @@
 #!/bin/sh
 
-set -e
+set -euo pipefail
 
 echo "Preparing env variables"
 
 file_env() {
-	local var="$1"
-	local fileVar="${var}_FILE"
-	local def="${2:-}"
-	local varValue=$(env | grep -E "^${var}=" | sed -E -e "s/^${var}=//")
-	local fileVarValue=$(env | grep -E "^${fileVar}=" | sed -E -e "s/^${fileVar}=//")
-	if [ -n "${varValue}" ] && [ -n "${fileVarValue}" ]; then
-		echo >&2 "error: both $var and $fileVar are set (but are exclusive)"
-		exit 1
-	fi
-	if [ -n "${varValue}" ]; then
-		export "$var"="${varValue}"
-	elif [ -n "${fileVarValue}" ]; then
-		export "$var"="$(cat "${fileVarValue}")"
-	elif [ -n "${def}" ]; then
-		export "$var"="$def"
-	fi
-	unset "$fileVar"
+    local var="$1"
+    local fileVar="${var}_FILE"
+    local def="${2:-}"
+    local varValue=$(env | grep -E "^${var}=" | sed -E -e "s/^${var}=//")
+    local fileVarValue=$(env | grep -E "^${fileVar}=" | sed -E -e "s/^${fileVar}=//")
+    if [ -n "${varValue}" ] && [ -n "${fileVarValue}" ]; then
+        echo >&2 "error: both $var and $fileVar are set (but are exclusive)"
+        exit 1
+    fi
+	export "$var"="$(cat "${fileVarValue}")"
+    unset "$fileVar"
 }
 
-env | grep "_FILE=/run/secrets" | while read -r line ; do
-    echo "Processing ${line%_FILE*}"
-    file_env "${line%_FILE*}"
-done
+while read -r line ; do
+	var="${line%_FILE*}"
+    echo "Processing $var"
+    file_env "$var"
+done < <(env | grep "_FILE=/run/secrets")
 
 exec "$@"
