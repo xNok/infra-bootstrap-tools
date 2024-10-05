d9eb9631d933f2850ce308bb53796091e3758a12
Alexandre Couedelo <nokwebspace@gmail.com>
2024-10-05 22:01:01 +0000

    fix: caddy varaible where using outdate environement

--- terraform/github-repository/action-caddy-swarm.tf
+++ terraform/github-repository/action-caddy-swarm.tf
@@ -12,7 +12,7 @@
  */
 resource "github_actions_environment_secret" "caddy_github_client_id" {
   repository       = data.github_repository.repo.name
-  environment      = github_repository_environment.digitalocean_environment.environment
+  environment      = github_repository_environment.docker_swarm.environment
   secret_name      = "CADDY_GITHUB_CLIENT_ID"
   lifecycle {
     # This resource is intended as placeholder chnaging the value after creation is ok
@@ -24,7 +24,7 @@ resource "github_actions_environment_secret" "caddy_github_client_id" {
 
 resource "github_actions_environment_secret" "caddy_github_client_secret" {
   repository       = data.github_repository.repo.name
-  environment      = github_repository_environment.digitalocean_environment.environment
+  environment      = github_repository_environment.docker_swarm.environment
   secret_name      = "CADDY_GITHUB_CLIENT_SECRET"
   lifecycle {
     # This resource is intended as placeholder chnaging the value after creation is ok
@@ -42,7 +42,7 @@ resource "tls_private_key" "caddy_jwt_shared_key" {
 
 resource "github_actions_environment_secret" "caddy_jwt_shared_key" {
   repository       = data.github_repository.repo.name
-  environment      = github_repository_environment.digitalocean_environment.environment
+  environment      = github_repository_environment.docker_swarm.environment
   secret_name      = "CADDY_JWT_SHARED_KEY"
   plaintext_value  = tls_private_key.caddy_jwt_shared_key.private_key_openssh
 
@@ -56,7 +56,7 @@ resource "github_actions_environment_secret" "caddy_jwt_shared_key" {
 
 resource "github_actions_environment_secret" "caddy_digitalocean_api_token" {
   repository       = data.github_repository.repo.name
-  environment      = github_repository_environment.digitalocean_environment.environment
+  environment      = github_repository_environment.docker_swarm.environment
   secret_name      = "CADDY_DIGITALOCEAN_API_TOKEN"
   plaintext_value  = ""
 
