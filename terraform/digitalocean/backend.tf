99cb4257f35306739a1f4e53499ec96c81283661
Alexandre Couedelo <nokwebspace@gmail.com>
2025-03-23 12:13:55 +0000

    chore: change backend for digitalocean tf

--- terraform/digitalocean/backend.tf
+++ terraform/digitalocean/backend.tf
@@ -1,10 +1,18 @@
 terraform {
-  backend "remote" {
-    hostname = "app.terraform.io"
-    organization = "nokwebspace"
-
-    workspaces {
-      name = "infra-bootstrap-tools-digitalocean"
+  backend "s3" {
+    endpoints = {
+      s3 = "https://infra-bootstrap-tools-terrafrom.nyc3.digitaloceanspaces.com"
     }
+
+    bucket = "infra-bootstrap-tools-terrafrom"
+    key    = "digitalocean"
+
+    # Deactivate a few AWS-specific checks
+    skip_credentials_validation = true
+    skip_requesting_account_id  = true
+    skip_metadata_api_check     = true
+    skip_region_validation      = true
+    skip_s3_checksum            = true
+    region                      = "us-east-1"
   }
 }
