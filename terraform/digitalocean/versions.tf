ee77496c6128607b3bba3135b8f0e5d2991b92db
Alexandre Couedelo <nokwebspace@gmail.com>
2025-03-23 12:18:54 +0000

    chore: minor cofig changes

--- terraform/digitalocean/versions.tf
+++ terraform/digitalocean/versions.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = "~>1.9"
+  required_version = "~> 1.11"
 
   required_providers {
 
@@ -31,6 +31,7 @@ provider "sshclient" {
 provider "digitalocean" {
   # Configuration options
 }
+
 provider "github" {
   # Configuration options
 }
